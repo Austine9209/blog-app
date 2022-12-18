6f2c19ab9456fe4842577c3707e007bf61776dc1
Austine9209 <austinebaraka9209@gmail.com>
2022-12-19 01:05:32 +0300

    Update Compose

--- src/pages/Compose.jsx
+++ src/pages/Compose.jsx
@@ -1,6 +1,18 @@
 import React, { useState, useEffect } from "react";
 import ReactTagInput from "@pathofdev/react-tag-input";
 import "@pathofdev/react-tag-input/build/index.css";
+import { db, storage } from "../firebase/firebase";
+import { useParams } from "react-router-dom";
+import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
+import {
+  addDoc,
+  collection,
+  getDoc,
+  serverTimestamp,
+  doc,
+  updateDoc,
+} from "firebase/firestore";
+import { toast } from "react-toastify";
 
 const initialState = {
   title: "",
@@ -17,8 +29,48 @@ const categoryOption = ["Isekai", "Action", "Drama", "Sports", "Mecha"];
 const Compose = () => {
   const [form, setForm] = useState(initialState);
   const [file, setFile] = useState(null);
+  const [progress, setProgress] = useState(null);
+
+  const { id } = useParams();
 
   const { title, tags, category, trending, description } = form;
+
+  useEffect(() => {
+    const uploadFile = () => {
+      const storageRef = ref(storage, file.name);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        "state_changed",
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          setProgress(progress);
+          switch (snapshot.state) {
+            case "paused":
+              console.log("Upload is paused");
+              break;
+            case "running":
+              console.log("Upload is running");
+              break;
+            default:
+              break;
+          }
+        },
+        (error) => {
+          console.log(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadUrl) => {
+            toast.info("Image upload to firebase successfully");
+            setForm((prev) => ({ ...prev, imgUrl: downloadUrl }));
+          });
+        }
+      );
+    };
+
+    file && uploadFile();
+  }, [file]);
 
   const handleChange = (e) => {
     setForm({ ...form, [e.target.name]: e.target.value });
@@ -86,19 +138,44 @@ const Compose = () => {
             </div>
           </div>
           <div>
-                <select
-                  value={category}
-                  onChange={onCategoryChange}
-                  className="category-dropdown"
-                >
-                  <option>Please select category</option>
-                  {categoryOption.map((option, index) => (
-                    <option value={option || ""} key={index}>
-                      {option}
-                    </option>
-                  ))}
-                </select>
-              </div>
+            <select
+              value={category}
+              onChange={onCategoryChange}
+              className="category-dropdown"
+            >
+              <option>Please select category</option>
+              {categoryOption.map((option, index) => (
+                <option value={option || ""} key={index}>
+                  {option}
+                </option>
+              ))}
+            </select>
+          </div>
+          <div>
+            <textarea
+              className="form-control description-box"
+              placeholder="Description"
+              value={description}
+              name="description"
+              onChange={handleChange}
+            />
+          </div>
+          <div>
+            <input
+              type="file"
+              className="form-control"
+              onChange={(e) => setFile(e.target.files[0])}
+            />
+          </div>
+          <div>
+            <button
+              className="btn-add"
+              type="submit"
+              disabled={progress !== null && progress < 100}
+            >
+              {id ? "Update" : "Submit"}
+            </button>
+          </div>
         </form>
       </div>
     </div>
