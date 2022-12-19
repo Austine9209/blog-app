81f35c4abf36e94b2b0082a8938c79d754e64350
Austine9209 <austinebaraka9209@gmail.com>
2022-12-19 21:02:55 +0300

    update Home

--- src/components/blogSection/BlogSection.jsx
+++ src/components/blogSection/BlogSection.jsx
@@ -1,7 +1,5 @@
-import React, { useEffect } from "react";
-import FontAwesome from "react-fontawesome";
+import React from "react";
 import { Link } from "react-router-dom";
-import { excerpt } from "../../utility";
 
 const BlogSection = ({
   id,
@@ -17,48 +15,33 @@ const BlogSection = ({
 }) => {
   return (
     <div>
-      <div className="row pb-4" key={id}>
-        <div className="col-md-5">
-          <div className="hover-blogs-img">
-            <div className="blogs-img">
-              <img src={imgUrl} alt={title} />
-              <div></div>
+      <div className="blog-section" key={id}>
+        <img className="blogSection-cover" src={imgUrl} alt={title} />
+        <h6 className="category">{category}</h6>
+        <h3>{title}</h3>
+        <p className="blogSection-desc">{(description, 120)}</p>
+        <footer>
+          <div className="blogSection-author">
+            {/* author avatar */}
+            <div>
+              <h6>{author}</h6>
+              <p>{timestamp.toDate().toDateString}</p>
             </div>
           </div>
-        </div>
-        <div className="col-md-7">
-          <div className="text-start">
-            <h6 className="category catg-color">{category}</h6>
-            <span className="title py-2">{title}</span>
-            <span className="meta-info">
-              <p className="author">{author}</p> -&nbsp;
-              {timestamp.toDate().toDateString()}
-            </span>
-          </div>
-          <div className="short-description text-start">
-            {excerpt(description, 120)}
-          </div>
-          <Link to={`/detail/${id}`}>
-            <button className="btn btn-read">Read More</button>
+          <Link className="blogSection-link" to={`/detail/${id}`}>
+            ➝
           </Link>
-          {user && user.uid === userId && (
+        </footer>
+        {user && user.uid === userId && (
             <div style={{ float: "right" }}>
-              <FontAwesome
-                name="trash"
-                style={{ margin: "15px", cursor: "pointer" }}
-                size="2x"
-                onClick={() => handleDelete(id)}
-              />
+              <button onClick={() => handleDelete(id)}></button>
               <Link to={`/update/${id}`}>
-                <FontAwesome
-                  name="edit"
+                <button
                   style={{ cursor: "pointer" }}
-                  size="2x"
-                />
+                  size="2x">Edit</button>
               </Link>
             </div>
           )}
-        </div>
       </div>
     </div>
   );
